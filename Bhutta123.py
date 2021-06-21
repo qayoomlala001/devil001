0a162661d0be491809e004c01d13150251484b61
qayoomlala001 <83922092+qayoomlala001@users.noreply.github.com>
2021-06-21 12:35:01 -0700

    Update Bhutta123.py

--- Bhutta123.py
+++ Bhutta123.py
@@ -659,14 +659,14 @@ except:
 
 p = ThreadPool(30)
 p.map(main, id)
-    print ''
-    print 47 * '-'
-    print ''
-    print ' \033[1;92mCrack Done'
-    print '\033[1;92m Total Ok/Cp:' + str(len(oks)) + '/' + str(len(cps))
-    print ''
-    print 47 * '-'
-    print ''
+print ''
+print 47 * '-'
+print ''
+print ' \033[1;92mCrack Done'
+print '\033[1;92m Total Ok/Cp:' + str(len(oks)) + '/' + str(len(cps))
+print ''
+print 47 * '-'
+print ''
     raw_input('\033[1;93m Press enter to back')
     choice_crack()
 
